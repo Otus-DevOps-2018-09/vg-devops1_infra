0d46ecb5cd9f58e6778d14145ebfda3ea9b3772f
vg-devops1 <v_k_special@yahoo.com>
2018-10-25 16:50:19 +0100

    updated scripts for reddit-full

---
+++ packer/scripts/deploy.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+cd ~
+git clone -b monolith https://github.com/express42/reddit.git
+cd reddit
+bundle install
+the_path_to_puma=$(which puma)
+the_to_reddit="/home/appuser/reddit/"
+the_path_to_script="/home/appuser/startpuma.sh"
+
+sudo tee $the_path_to_script <<EOF
+#! /bin/bash
+cd /home/appuser/reddit/ 
+$the_path_to_puma -d
+EOF
+
+sudo chmod +x $the_path_to_script
+
+sudo tee /etc/systemd/system/mypuma.service <<EOF
+[Unit]
+Description=Puma Service
+Requires=network-online.target
+After=network-online.target
+
+[Service]
+User=appuser
+RemainAfterExit=yes
+ExecStart=$the_path_to_script
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl enable mypuma.service
